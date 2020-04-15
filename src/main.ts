ea2146bd30e44a360bfd61446c714e1d6ef23848
^,^ <joy.void.joy@gmail.com>
2020-04-15 17:47:49 +0200

    feat: add memory settings to template

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,7 @@ export let FMODStudio = null
 
 const FMOD: FMOD_ = 
 {
-  TOTAL_MEMORY: 64 * 1024 * 1024,
+  TOTAL_MEMORY: 128 * 1024 * 1024,
 
   preRun()
   {
@@ -54,10 +54,33 @@ const FMOD: FMOD_ =
     
     FMOD.Studio_System_Create(out)
     FMODStudio = out.val
-    FMODStudio.initialize(128, FMOD.STUDIO_INIT_NORMAL, FMOD.INIT_NORMAL, null)
+    // More advanced settings to tweak the memory gestion
+    FMODStudio.setAdvancedSettings(
+    {
+      //Command queue size for studio async processing.
+      commandqueuesize : 32768,
+
+      //Initial size to allocate for handles. Memory for handles will grow as needed in pages.
+      handleinitialsize : 32768,
+
+      //Update period of Studio when in async mode, in milliseconds. Will be quantized to the nearest multiple of mixer duration.
+      studioupdateperiod : 20,
+
+      //Size in bytes of sample data to retain in memory when no longer used, to avoid repeated disk I/O. Use -1 to disable.
+      idlesampledatapoolsize : 262144,
+
+      //Specify the schedule delay for streams, in samples. Lower values can reduce latency when scheduling events containing streams but may cause scheduling issues if too small.
+      streamingscheduledelay : 8192,
+    })
+    FMODStudio.initialize(1024, STUDIO_INITFLAGS.NORMAL, INITFLAGS.NORMAL, null)
+
+
 
     // Loading banks, so that we can use getEvent normally after
-    banks.map(i => FMODStudio.loadBankFile(i, FMOD.STUDIO_LOAD_BANK_NORMAL, {}))
+    banks.map(i => FMODStudio.loadBankFile(i, STUDIO_LOAD_BANK_FLAGS.NORMAL | STUDIO_LOAD_BANK_FLAGS.DECOMPRESS_SAMPLES, {}))
+    FMODStudio.getBank("bank:/Music", out)
+    out.val.loadSampleData()
+
 
     // Finally run game
     const game = new Phaser.Game(config)
